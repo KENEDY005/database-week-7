e59e57121941f53c1344ccd983b1f241aaaf8347
Kenedy005 <kenedyambilah@gmail.com>
2025-09-18 12:18:53 +0300

    assignment submission

---
+++ answers.sql
@@ -0,0 +1,49 @@
+-- Q1 Creating a new normalized table
+use salesdb;
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(50)
+);
+
+--  Inserting data into the new table (splitting products into rows)
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+
+-- Q2: CreatING a separate Orders table (CustomerName depends only on OrderID)
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- Insert unique Order data
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+
+--  Creating an OrderProducts table (products and their quantities)
+CREATE TABLE OrderProducts (
+    OrderID INT,
+    Product VARCHAR(50),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Insert product details for each order
+INSERT INTO OrderProducts (OrderID, Product, Quantity)
+VALUES
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
